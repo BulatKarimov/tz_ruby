4b23f5a4d79f853aafda24c6f5512697f2da41f1
Булат Каримов <bulatkarimov96@gmail.com>
2019-09-22 20:44:02 +0300

    [fix] solve method

--- app/controllers/api/v1/solutions_controller.rb
+++ app/controllers/api/v1/solutions_controller.rb
@@ -1,16 +1,24 @@
 class Api::V1::SolutionsController < Api::V1::ApplicationController
 
   def solve
-    result = SolveExample.call(a: params[:a],
-                              b: params[:b],
-                              c: params[:c],
-                              d: params[:d],
-                              type: params[:type])
+    result = SolveExample.call(set_solve_params)
 
     if result.success?
       render json: { result: result.result }, status: :ok
     else
       render json: { message: result.error }, status: :unprocessable_entity
     end
+  end
+
+  private 
+
+  def set_solve_params
+    {
+      a: params[:a].to_i,
+      b: params[:b].to_i,
+      c: params[:c].to_i,
+      d: params[:d].to_i,
+      type: params[:type]
+    }
   end
 end
--- app/interactors/check_params.rb
+++ app/interactors/check_params.rb
@@ -6,12 +6,12 @@ class CheckParams
   end
 
   def call
-    context.fail!(error: 'A param is missing or must be integer') if context.a.nil? || !(context.a.is_a? Integer)
-    context.fail!(error: 'B param is missing or must be integer') if context.b.nil? || !(context.b.is_a? Integer)
-    context.fail!(error: 'C param is missing or must be integer') if context.c.nil? || !(context.c.is_a? Integer)
+    context.fail!(error: 'A param is missing or must be integer') if context.a.nil?
+    context.fail!(error: 'B param is missing or must be integer') if context.b.nil?
+    context.fail!(error: 'C param is missing or must be integer') if context.c.nil?
 
     if context.type == 'sqrt'
-      context.fail!(error: 'D param is missing or must be integer') if context.d.nil? || !(context.d.is_a? Integer)
+      context.fail!(error: 'D param is missing or must be integer') if context.d.nil?
     elsif context.type != 'lin' && context.type != 'sqrt'
       context.fail!(error: "Type must be 'sqrt' of 'lin'")
     end
