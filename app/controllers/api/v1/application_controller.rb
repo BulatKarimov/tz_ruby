a754f7727b3ea6b8dc2c0d6dc6dcebe32427c0a4
Булат Каримов <bulatkarimov96@gmail.com>
2019-09-22 20:56:07 +0300

    [add] simple auth\

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,4 +1,10 @@
 class Api::V1::ApplicationController < Api::ApplicationController
   skip_before_action :verify_authenticity_token
-  
+  before_action :check_auth
+
+  def check_auth
+    unless request.headers['Authorization'] == "Bearer #{ENV['AUTH_TOKEN']}"
+      return head 401
+    end
+  end
 end
