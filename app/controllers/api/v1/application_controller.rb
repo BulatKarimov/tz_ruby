2be07f3637bc5431a34111cc1589410d5f721380
Булат Каримов <bulatkarimov96@gmail.com>
2019-09-24 21:51:12 +0300

    [tmp] remove check_auth

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ApplicationController < Api::ApplicationController
   skip_before_action :verify_authenticity_token
-  before_action :check_auth
+  #before_action :check_auth
 
   def check_auth
     unless request.headers['Authorization'] == "Bearer #{ENV['AUTH_TOKEN']}"
